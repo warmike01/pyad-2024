3d7989f6e5f29030e22ad5e00a02d366dedff6ee
anastasia_cher <anastasiia.cher@yandex.ru>
2024-10-04 18:15:33 +0300

    Update test_basic_math.py

--- lab0/test_basic_math.py
+++ lab0/test_basic_math.py
@@ -72,7 +72,7 @@ class MATHTestCase(unittest.TestCase):
         x1 = [2,3,5,7,8]
         x2 = [2,3,2,5,7,2,2,8]
         self.assertEqual(basic_math.skew(x1), round(scipy.stats.skew(x1), 2))
-        self.assertEqual(basic_math.skew(x1), round(scipy.stats.skew(x2), 2))
+        self.assertEqual(basic_math.skew(x2), round(scipy.stats.skew(x2), 2))
 
         random.seed(100)
         random_floats = [random.random() for _ in range(10000)]
@@ -85,7 +85,7 @@ class MATHTestCase(unittest.TestCase):
         x1 = [2,3,5,7,8]
         x2 = [2,3,2,5,7,2,2,8]
         self.assertEqual(basic_math.kurtosis(x1), round(scipy.stats.kurtosis(x1), 2))
-        self.assertEqual(basic_math.kurtosis(x1), round(scipy.stats.kurtosis(x2), 2))
+        self.assertEqual(basic_math.kurtosis(x2), round(scipy.stats.kurtosis(x2), 2))
 
         random.seed(100)
         random_floats = [random.random() for _ in range(10000)]
