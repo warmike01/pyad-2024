e8c5081facc6d1e11f3aa7a5f55c2dc347863005
Mikhail <368306@niuitmo.ru>
2024-10-20 23:12:21 +0300

    tasks 1, 2

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -1,15 +1,23 @@
-import numpy as np
-import scipy as sc
-
-
-def matrix_multiplication(matrix_a, matrix_b):
+def matrix_multiplication(matrix1, matrix2):
     """
     Задание 1. Функция для перемножения матриц с помощью списков и циклов.
     Вернуть нужно матрицу в формате списка.
     """
-    # put your code here
-    pass
-
+    if len(matrix1[0]) != len(matrix2):
+        #перемножить нельзя
+        return None
+    else:
+        result = []
+        rows=len(matrix1)
+        columns=len(matrix2)
+        for i in range(rows):
+            result.append([])
+            for j in range(rows):
+                result[i].append(0)
+                for k in range(columns):
+                    result[i][j] += matrix1[i][k] * matrix2[k][j]
+        return result
+#print(matrix_multiplication([[1,2,3], [4,5,6]], [[1,2], [3,4], [5,6]]))
 
 def functions(a_1, a_2):
     """
@@ -17,8 +25,49 @@ def functions(a_1, a_2):
     Необходимо найти точки экстремума функции и определить, есть ли у функций общие решения.
     Вернуть нужно координаты найденных решения списком, если они есть. None, если их бесконечно много.
     """
-    # put your code here
-    pass
+    inp=a_1.split()
+    a1=int(inp[0])
+    b1=int(inp[1])
+    c1=int(inp[2])
+    inp=a_2.split()
+    a2=int(inp[0])
+    b2=int(inp[1])
+    c2=int(inp[2])
+    if a1==a2 and b1==b2 and c1==c2:
+        return None
+    elif a1==0 and a2==0:
+            if b1 == b2:
+                return []
+            else: 
+                sol_x = (c2-c1)/(b1-b2)
+                sol_y = sol_x * b1 + c1
+                return [tuple([sol_x, sol_y])]
+    else:
+        a_t = a1-a2
+        b_t=b1-b2
+        c_t=c1-c2
+        if a_t == 0:
+            if b_t == 0:
+                return []
+            else: 
+                sol_x = -c_t / b_t
+                sol_y = sol_x *sol_x*a1 + sol_x*b1 + c1
+                return [tuple([sol_x, sol_y])]
+        else:
+            D_t=b_t * b_t - 4 * a_t * c_t
+            if D_t < 0:
+                return []
+            elif D_t == 0:
+                sol_x = -b_t / 2 / a_t
+                ol_y = sol_x *sol*x*a1 + sol_x*b1 + c1
+                return [tuple([sol_x, sol_y])]
+            else:
+                sol1_x= (D_t ** 0.5 - b_t) / 2 / a_t
+                sol1_y = sol1_x *sol1_x*a1 + sol1_x*b1 + c1
+                sol2_x= (-D_t ** 0.5 - b_t) / 2 / a_t
+                sol2_y = sol2_x *sol2_x*a1 + sol2_x*b1 + c1
+                return [tuple([sol1_x, sol1_y]), tuple([sol2_x, sol2_y])]
+
 
 
 def skew(x):
